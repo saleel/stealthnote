3feae2690c36d19b2440e22f16437db65f86c5a4
Saleel <saleel@saleel.xyz>
2024-09-16 16:18:37 +0530

    chore: remove react app

--- app/src/main.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import { Buffer } from "buffer";
-import "./styles.css";
-import App from "./app.tsx";
-
-globalThis.Buffer = Buffer;
-
-createRoot(document.getElementById("root")!).render(<App />);
