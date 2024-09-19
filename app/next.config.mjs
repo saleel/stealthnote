435ddae2ac1db23bd47bb2402f0877f2b86f41b1
Saleel <saleel@saleel.xyz>
2024-09-19 13:15:37 +0530

    app: add to outputFileTracingIncludes

--- app/next.config.mjs
+++ app/next.config.mjs
@@ -7,7 +7,9 @@ const nextConfig = {
   experimental: {
     outputFileTracingIncludes: {
       '/api/messages': [
-        'node_modules/@aztec/bb.js/dest/node/barretenberg_wasm/*',
+        'node_modules/@aztec/bb.js/dest/node/barretenberg_wasm/barretenberg-threads.wasm',
+        'node_modules/@aztec/bb.js/dest/node/barretenberg_wasm/barretenberg_wasm_main/factory/node/main.worker.js',
+        '/node_modules/@aztec/bb.js/dest/node/barretenberg_wasm/barretenberg_wasm_thread/factory/node/thread.worker.js'
       ],
     },
   },
