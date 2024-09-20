8f1737ffa111eb2a1588b6df0e45804cd4cd5aee
Saleel <saleel@saleel.xyz>
2024-09-20 15:25:49 +0530

    app: add how it works section

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -2,27 +2,34 @@
 
 import React from "react";
 import SignInButton from "../components/siwg";
+import Head from "next/head";
 
 export default function Home() {
   return (
-    <div className="page">
-      <main className="intro">
-        <h1 className="intro-title">Welcome to StealthNote</h1>
-        <p>
-          StealthNote is an application for people in an organization to
-          anonymously broadcast messages.
-        </p>
-        <p>
-          We use Zero Knowledge Proofs to prove that you are part of an
-          organization without revealing any information about yourself.
-        </p>
-        <p>
-          Sign in with your <u>work Google account</u> (Google Workspace) to get
-          started.
-        </p>
+    <>
+      <Head>
+        <title>StealthNote</title>
+      </Head>
 
-        <SignInButton />
-      </main>
-    </div>
+      <div className="page">
+        <main className="intro">
+          <h1 className="intro-title">Welcome to StealthNote</h1>
+          <p>
+            StealthNote is an application for people in an organization to
+            anonymously broadcast messages.
+          </p>
+          <p>
+            We use Zero Knowledge Proofs to prove that you are part of an
+            organization without revealing any information about yourself.
+          </p>
+          <p>
+            Sign in with your <u>work Google account</u> (<span className="inline-code">you@company.com</span>) to
+            get started.
+          </p>
+
+          <SignInButton />
+        </main>
+      </div>
+    </>
   );
 }
