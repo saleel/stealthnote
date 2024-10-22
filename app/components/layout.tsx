65a39e176d48f04bf3a297b141caec54ab9e7ffd
Saleel <saleel@saleel.xyz>
2024-10-22 08:53:53 +0100

    feat: responsive domain page

--- app/components/layout.tsx
+++ app/components/layout.tsx
@@ -5,50 +5,9 @@ import Link from "next/link";
 import dynamic from "next/dynamic";
 import { useLocalStorage } from "@uidotdev/usehooks";
 
-function Sidebar() {
-  const [currentDomain] = useLocalStorage<string | null>(
-    "currentDomain",
-    null
-  );
-
-  return (
-    <>
-      <div className="logo">
-        <Link href="/">StealthNote</Link>
-      </div>
-      <nav className="sidebar-nav">
-        <Link href="/" className="sidebar-nav-item">
-          Home
-        </Link>
-
-        {currentDomain && (
-          <Link
-            href={`/internal/${currentDomain}`}
-            className="sidebar-nav-item"
-          >
-            {currentDomain} Internal
-          </Link>
-        )}
-
-        <div className="sidebar-nav-footer">
-          <Link href="/how-it-works" className="sidebar-nav-item">
-            How It Works
-          </Link>
-          <Link
-            className="sidebar-nav-item"
-            target="_blank"
-            rel="noopener noreferrer"
-            href="https://github.com/saleel/stealthnote"
-          >
-            Github
-          </Link>
-        </div>
-      </nav>
-    </>
-  );
-}
-
 const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+  const [currentDomain] = useLocalStorage<string | null>("currentDomain", null);
+
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
 
   const toggleSidebar = () => {
@@ -58,7 +17,10 @@ const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   return (
     <div className="page">
       <div className="mobile-header">
-        <button className={`sidebar-toggle ${isSidebarOpen ? 'open' : ''}`} onClick={toggleSidebar}>
+        <button
+          className={`sidebar-toggle ${isSidebarOpen ? "open" : ""}`}
+          onClick={toggleSidebar}
+        >
           ☰
         </button>
         <div
@@ -68,8 +30,44 @@ const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
           <Link href="/">StealthNote</Link>
         </div>
       </div>
-      <aside className={`sidebar ${isSidebarOpen ? 'open' : ''}`}>
-        <Sidebar />
+      <aside className={`sidebar ${isSidebarOpen ? "open" : ""}`}>
+        <div className="logo">
+          <Link href="/">StealthNote</Link>
+        </div>
+        <nav className="sidebar-nav">
+          <Link onClick={toggleSidebar} href="/" className="sidebar-nav-item">
+            Home
+          </Link>
+
+          {currentDomain && (
+            <Link
+              onClick={toggleSidebar}
+              href={`/internal/${currentDomain}`}
+              className="sidebar-nav-item"
+            >
+              {currentDomain} Internal
+            </Link>
+          )}
+
+          <div className="sidebar-nav-footer">
+            <Link
+              onClick={toggleSidebar}
+              href="/how-it-works"
+              className="sidebar-nav-item"
+            >
+              How It Works
+            </Link>
+            <Link
+              onClick={toggleSidebar}
+              className="sidebar-nav-item"
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://github.com/saleel/stealthnote"
+            >
+              Github
+            </Link>
+          </div>
+        </nav>
       </aside>
       <main className="container">
         <div className="content">{children}</div>
