e57636285cfcde358b60216d324c63a11f92de50
Saleel <saleel@saleel.xyz>
2024-11-24 23:42:35 +0400

    feat: add multi-thread

--- app/next.config.mjs
+++ app/next.config.mjs
@@ -49,6 +49,25 @@ const nextConfig = {
     );
     return config
   },
+  async headers() {
+    // These headers are necessary to enabled SharedArrayBuffer 
+    // which is needed for multi-threaded proof generation
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Cross-Origin-Embedder-Policy',
+            value: 'require-corp',
+          },
+          {
+            key: 'Cross-Origin-Opener-Policy',
+            value: 'same-origin',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
