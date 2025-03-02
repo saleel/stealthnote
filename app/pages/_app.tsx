7580f716e837f3ddafbc6fd1745c22b14622e2d4
saleel <saleel@saleel.xyz>
2025-03-02 13:35:36 +0400

    feat: add vercel analytics

--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -2,6 +2,7 @@ import type { AppProps } from 'next/app'
 import '../styles/main.scss'
 import Layout from '../components/layout'
 import TimeAgo from "javascript-time-ago";
+import { Analytics } from '@vercel/analytics/next';
 import en from "javascript-time-ago/locale/en.json";
 
 TimeAgo.addDefaultLocale(en);
@@ -10,6 +11,7 @@ export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Layout>
       <Component {...pageProps} />
+      <Analytics />
     </Layout>
   )
 }
